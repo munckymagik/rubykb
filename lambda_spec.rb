0dfc01b0673e236c6c4202a72340a84b3711a0f2
Dan Munckton <dangit@munckfish.net>
2013-12-26 11:02:20 +0000

    Create initial spec for Ruby's lambda

---
+++ lambda_spec.rb
@@ -0,0 +1,64 @@
+describe "lambda" do
+
+  it "returns a Proc" do
+    # given
+    inc = lambda { |x| x + 1 }
+
+    # then
+    expect(inc).to be_an_instance_of(Proc)
+  end
+
+  it "can be defined using the special syntax ->() {}" do
+    # given
+    inc = ->(x) { x + 1 }
+
+    # then
+    expect(inc).to be_an_instance_of(Proc)
+    expect(inc).to be_a_lambda
+  end
+
+  it "can be invoked by calling #call" do
+    # given
+    inc = lambda { |x| x + 1 }
+
+    # when
+    y = inc.call(99)
+
+    # then
+    expect(y).to eq(100)
+  end
+
+  it "cannot be invoked like a function using normal braces" do
+    # given
+    inc = lambda { |x| x + 1 }
+
+    # then
+    expect {
+      # when
+      # this tries to call 'inc' on the enclosing scope
+      y = inc(99)
+    }.to raise_error(NoMethodError)
+  end
+
+  it "can be invoked by calling as instance.(...)" do
+    # given
+    inc = lambda { |x| x + 1 }
+
+    # when
+    y = inc.(99)
+
+    # then
+    expect(y).to eq(100)
+  end
+
+  it "can be invoked using square brackets" do
+    # given
+    inc = lambda { |x| x + 1 }
+
+    # when
+    y = inc[99]
+
+    # then
+    expect(y).to eq(100)
+  end
+end
