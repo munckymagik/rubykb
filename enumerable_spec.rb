16dc22c6ca2227c142114942407274e9363c23be
Dan Munckton <daniel.munckton@neo.com>
2014-01-24 15:55:00 +0000

    Move the enumerable examples out into a separate file

---
+++ enumerable_spec.rb
@@ -0,0 +1,39 @@
+describe Enumerable do
+  example "It is mixed into Array, Hash and several other classes" do
+    expect(Array.include? Enumerable).to be_true
+    expect(Hash.include? Enumerable).to be_true
+    expect(Range.include? Enumerable).to be_true
+    expect(Dir.include? Enumerable).to be_true
+    expect(Struct.include? Enumerable).to be_true
+    expect(IO.include? Enumerable).to be_true
+  end
+
+  example "the including class must define #each" do
+    expect(Array.new.respond_to? :each).to be_true
+    expect(Hash.new.respond_to? :each).to be_true
+    expect(Range.new(0,0,0).respond_to? :each).to be_true
+  end
+
+  class FakeCollection
+    include Enumerable
+    def each
+      yield 1
+      yield 2
+      yield 3
+    end
+  end
+
+  example "It provides the important common collection methods" do
+    fc = FakeCollection.new
+    expect(fc.map { |x| 2*x }).to eq([2, 4, 6])
+    expect(fc.select { |x| x > 1 }).to eq([2, 3])
+    # Note count, counts by enumerating so it will be O(n)
+    expect(fc.count).to eq(3)
+    expect(fc.map { |x| [x, x*2] }).to eq([[1, 2], [2, 4], [3, 6]])
+    expect(fc.flat_map { |x| [x, x*2] }).to eq([1, 2, 2, 4, 3, 6])
+    expect(fc.include? 2).to be_true
+    expect(fc.reduce(0) { |accum, x| accum + x}).to eq(6)
+    expect(fc.any? { |x| x % 2 == 0 }).to be_true
+    # and so on ...
+  end
+end
