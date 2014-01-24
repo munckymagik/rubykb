b0f4a9b1d18b3cc6103dcba6071cb308e2e5c439
Dan Munckton <daniel.munckton@neo.com>
2014-01-24 16:24:49 +0000

    Reformat proc_spec and mark sections as pending

--- proc_spec.rb
+++ proc_spec.rb
@@ -1,4 +1,5 @@
 describe Proc do
+
   context "creation" do
     describe "::new" do
       context "when passed a block" do
@@ -9,17 +10,20 @@ describe Proc do
           expect(prc.call).to eq("a direct block")
         end
       end
+
       context "when passed no block" do
         def create_proc_with_block
           # Implicitly uses factory method's block
           Proc.new
         end
+
         it "creates a proc using the block of the enclosing method" do
           prc = create_proc_with_block { "an indirect block" }
 
           expect(prc).to be_an_instance_of Proc
           expect(prc.call).to eq("an indirect block")
         end
+
         it "raises an ArgumentError when passed nothing" do
           expect {
             Proc.new
@@ -27,6 +31,7 @@ describe Proc do
         end
       end
     end
+
     describe "Kernel::proc" do
       it "is a equivalent to Proc::new" do
         prc = proc { "hello" }
@@ -50,35 +55,42 @@ describe Proc do
         result = proc { "hello" }.call
         expect(result).to eq("hello")
       end
+
       it "sets the block parameters to the values given in its params" do
         prc = proc { |x, y, *z| "#{x}, #{y}, #{z}" }
         expect(prc.call 1, 2, 3, 4).to eq("1, 2, [3, 4]")
       end
+
       it "silently discards extra parameters if created using Proc::new or Kernel::new" do
         prc = proc { |x| x }
         expect(prc.call(1, 2)).to eq(1)
       end
+
       it "provides nil for missing arguments" do
         prc = proc { |x, y| [x, y] }
         expect(prc.call(1)).to eq([1, nil])
       end
+
       it "expands a single array argument" do
         prc = proc { |x, y| [x, y] }
         expect(prc.call([1, 2])).to eq([1, 2])
         expect(prc.call([1, 2], 3)).to eq([[1, 2], 3])
       end
     end
+
     describe "#[]" do
       it "is an alias for #call" do
         prc = proc {}
         expect(prc.method :[]).to eq(prc.method :call)
       end
     end
+
     describe "#()" do
       it "is syntax sugar that invokes #call" do
         prc = proc { |x, y, *z| "#{x}, #{y}, #{z}" }
         expect(prc.(1, 2, 3, 4)).to eq("1, 2, [3, 4]")
       end
+
       it "is not listed as a member of #methods" do
         prc = proc {}
         expect(prc.methods.include? :"()").to be_false
@@ -93,23 +105,28 @@ describe Proc do
         expect(prc.to_proc).to be(prc)
       end
     end
+
     describe "#curry" do
       let(:prc) { proc { |x, y, z| x + y + z } }
+
       it "returns a curried proc that executes the original" do
         curried_proc = prc.curry
         expect(curried_proc).not_to be(prc)
         expect(curried_proc.call(1, 2, 3)).to eq(prc.call(1, 2, 3))
       end
+
       describe "the curried proc" do
         it "invokes the original proc only if enough arguments are passed" do
           curried_proc = prc.curry.call(1, 2)
           expect(curried_proc).to be_an_instance_of Proc
         end
+
         it "otherwise returns another curried proc that takes the rest of the arguments" do
           curried_proc = prc.curry.call(1, 2)
           expect(curried_proc.call(3)).to eq(6)
         end
       end
+
       describe "the optional arity argument" do
         it "specifies how many arguments should be expected before invoking the original" do
           curried_proc = prc.curry(4)[1, 2, 3]
@@ -121,17 +138,17 @@ describe Proc do
   end
 
   context "comparison" do
-
+    pending
   end
 
   context "properties" do
-    describe "#==="
-    describe "#arity"
-    describe "#inspect"
-    describe "#binding"
-    describe "#hash"
-    describe "#lambda?"
-    describe "#parameters"
-    describe "#source_location"
+    describe "#===" do pending end
+    describe "#arity" do pending end
+    describe "#inspect" do pending end
+    describe "#binding" do pending end
+    describe "#hash" do pending end
+    describe "#lambda?" do pending end
+    describe "#parameters" do pending end
+    describe "#source_location" do pending end
   end
 end
