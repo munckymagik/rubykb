0fcbe0e833057a8ce8cc2cb4fbc0ad7c111d1b36
Dan Munckton <daniel.munckton@neo.com>
2014-04-02 14:55:11 +0100

    Add a sketchy spec for the 'include' method

--- mixins_spec.rb
+++ mixins_spec.rb
@@ -30,4 +30,67 @@ describe "Mixins" do
   end
 
   example "Method and variable overriding rules"
+
+  describe "include" do
+
+    context "Modules" do
+      module A
+        CONSTANT = 1
+
+        def self.static_method
+          :hi
+        end
+
+        def inst_method
+          :bye
+        end
+      end
+
+      module B
+        include A
+      end
+
+      class C
+        include A
+      end
+
+      specify "module A has a static method and an instance method" do
+        expect(A.static_method).to eq(:hi)
+        expect {
+          A.inst_method
+        }.to raise_error(NoMethodError)
+      end
+
+      specify "module A is included in module B" do
+        expect(B.include? A).to be_true
+      end
+
+      specify "but that doesn't add anything useful to B" do
+        expect {
+          B.static_method
+        }.to raise_error(NoMethodError)
+        expect {
+          B.inst_method
+        }.to raise_error(NoMethodError)
+      end
+    end
+
+    context "Classes" do
+      specify "class C includes module A via module B" do
+        expect(C.include? A).to be_true
+      end
+
+      specify "class C inherits module A's instance methods" do
+        expect(C.new.inst_method).to eq(:bye)
+      end
+
+      specify "but class C does not inherit module A's class methods" do
+        expect {
+          C.static_method
+        }.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  describe "extend"
 end
